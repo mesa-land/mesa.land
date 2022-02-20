73811c425c737499eb58a596e9206155bc667c6f
Guilherme Rodrigues <gadr90@gmail.com>
2022-02-20 18:25:58 -0300

    Improve landing

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -2,18 +2,22 @@
 import { tw } from "../deps.ts";
 
 export const Welcome = () => (
-  <div class={tw`bg-indigo-50 bg-opacity-50 p-4 rounded-lg`}>
-    <h1 class={tw``}>Welcome to Mesa.land!</h1>
+  <div class={tw`bg-white bg-opacity-50 p-4 rounded-lg pt-6 mt-4 p-12`}>
+    <h2 class={tw`pt-0`}>Welcome to Mesa!</h2>
+    
+    <div class={tw`text-lg`}>
     <p>Mesa.land is a community-driven, extensible card game that's created by gamers, for gamers, like you.</p>
 
-    <p>The Mesa.land engine has a simple basic set of rules, but the game possibilities are infinite thanks to user-created cards.</p>
+    <p>The Mesa.land engine basic set of rules is simple, but the game possibilities are infinite thanks to user-created cards.</p>
 
-    <p>Play online for free or start creating your own cards.</p>
+    <h3>Start now</h3>
+    <p>Play online now or create your own cards. Made a great card? Sell it on the Market for fun and profit!</p>
+    </div>
 
-    <p>Made a great card? Sell it on the Marketplace and...? profit!</p>
-
-    <a href="/t/alpha">  
-      <button class={tw`bg-indigo-800 text-white text-bold`}>Play Alpha</button>
-    </a>
+    <div class={tw`w-full flex flex-row justify-center mt-8`}>
+      <a href="/t/alpha">  
+        <button class={tw`bg-purple-800 border-white border-opacity-50 rounded-lg p-4 text-white font-bold text-2xl `}>Play Alpha</button>
+      </a>
+    </div>
   </div>
 )
